e281e0a41a75578397b125d6f0e245708b7a3929
Thomas Boerger <thomas@webhippie.de>
2020-07-21 15:20:25 +0200

    Add version flag (#189)
    To properly check which version of the exporter had been installed from
    a binary it helps a lot to support the --version flag as the other
    exporters. It's just a one-line fix to print the version information on
    the CLI.
    
    Signed-off-by: Thomas Boerger <thomas@webhippie.de>

--- haproxy_exporter.go
+++ haproxy_exporter.go
@@ -563,6 +563,7 @@ func main() {
 
 	promlogConfig := &promlog.Config{}
 	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	kingpin.Version(version.Print("haproxy_exporter"))
 	kingpin.HelpFlag.Short('h')
 	kingpin.Parse()
 	logger := promlog.New(promlogConfig)
